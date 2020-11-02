e6f3636245a7fabefaacb2abf6a3bf5ff0e09208
Robert Gordon <Komeuppance@gmail.com>
2020-11-01 18:27:11 -0800

    Added packages

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/robert/go_crash_course/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutil.Reverse("olleH"))
 }
