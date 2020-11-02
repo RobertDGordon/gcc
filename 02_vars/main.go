2d491908b0baf9ae0204a0f0db54af82e456bb57
Robert Gordon <Komeuppance@gmail.com>
2020-11-01 18:06:48 -0800

    Added 03_packages

--- 02_vars/main.go
+++ 02_vars/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var name string = "Robert"
+	var age int = 36
+	const isCool = true
+
+	lastname, size := "Gordon", 1.2
+
+	fmt.Println(name, lastname, age, isCool)
+	fmt.Printf("%T\n", size)
 }
